1f35cbd926fae1c9459f46298f80c886d85864fa
morisum <moris@r.sannet.ne.jp>
2024-09-08 08:49:48 +0900

    Initial Commit

---
+++ TimerTest1/TimerTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTest1App.swift
+//  TimerTest1
+//
+//  Created by MsMacM on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct TimerTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
